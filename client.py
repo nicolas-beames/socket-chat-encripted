f2d98707cba2be0b6a67267fa9e21103e7284e32
nicolas-beames <nicolas.beames04@gmai.com>
2025-05-29 17:39:52 -0300

    final

--- client.py
+++ client.py
@@ -1,17 +1,18 @@
 import socket
 import threading
 
-nickname = input("Choose a nickname: ")
 
+nickname = input("Digite seu username: ")
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 55555))
+
 
 def receive():
     while True:
         try:
-            message = client.recv(1024).decode('ascii')
+            message = client.recv(1024).decode('utf-8')
             if message == 'NICK':
-                client.send(nickname.encode('ascii'))
+                client.send(nickname.encode('utf-8'))
             else:
                 print(message)
         except:
@@ -21,8 +22,13 @@ def receive():
 
 def write():
     while True:
+        inp = input("")
         message = f'{nickname}: {input("")}'
-        client.send(message.encode('ascii'))
+        client.send(message.encode("utf-8"))
+
+        if inp.lower() == "sair":
+            client.close()
+            break
 
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import threading
 import socket
 
+
 host = '127.0.0.1'
 port = 55555
 
@@ -11,7 +12,13 @@ server.listen()
 clients = []
 nicknames = []
 
+
 def broadcast(message):
+    # checka se a lista de clientes está vazia
+    # caso esteja, não há broadcast, apenas exibe
+    # a mensagem para o Servidor
+    print(message)
+    # envia a mensagem para todos na lista de clientes
     for client in clients:
         client.send(message)
 
@@ -19,32 +26,56 @@ def handle(client):
     while True:
         try:
             message = client.recv(1024)
+            if not message:
+                raise Exception(f'Usuário {client} desconectado!')
+            if message.decode("utf-8").endswith("exit"):
+                raise Exception(f'Usuário {client} saiu do chat!')
             broadcast(message)
+            print(message)
         except:
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            nickname = nicknames[index]
-            broadcast(f'{nickname} left the chat!'.encode('ascii'))
-            nicknames.remove(nickname)
+            if client in clients:
+                index = clients.index(client)
+                clients.remove(client)
+                client.close()
+                nickname = nicknames[index]
+                broadcast(f"{nickname.decode('utf-8')} saiu do chat ;-;".encode("ascii"))
+                nicknames.remove(nickname)
             break
 
 def receive():
     while True:
         client, address = server.accept()
-        print(f"Connected with {str(address)}")
+        print(f"Conectado com {str(address)}")
 
-        client.send('NICK'.encode('ascii'))
-        nickname = client.recv(1024).decode('ascii')
+        client.send('NICK'.encode('utf-8'))
+        nickname = client.recv(1024).decode('utf-8')
         nicknames.append(nickname)
         clients.append(client)
 
-        print(f"Nickname of the client is {nickname}!")
-        broadcast(f'{nickname} joined the chat!'.encode('ascii'))
-        client.send('Connected to the server!'.encode('ascii'))
+        print(f"Apelido: {nickname}!")
+        broadcast(f'{nickname} entrou no chat!'.encode('utf-8'))
+        client.send('Conectado!'.encode('utf-8'))
 
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
 
-print("Server is listening...")
-receive()
+def write():
+    while True:
+        message = f'Server: {input("")}'
+        broadcast(message.encode('utf-8'))
+        print(message)
+
+print("Servidor operando!")
+
+
+try:
+    receive()
+    write_thread = threading.Thread(target=write)
+    write_thread.start()
+except KeyboardInterrupt:
+    print("Desligando...")
+    broadcast("Servidor foi tirado do ar")
+    for client in clients:
+        client.close()
+    server.close()
+    print("Servidor offline")
